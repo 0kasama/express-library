a2227d70f022d7b29fb579f8cbeaed76aceda926
0kasama <okaajyad@gmail.com>
2024-12-12 13:45:16 +0700

    chore: update tables id default value

--- public/database.sql
+++ public/database.sql
@@ -13,7 +13,7 @@ $$ LANGUAGE plpgsql;
 
 
 CREATE TABLE "books" (
-  "id" UUID PRIMARY KEY,
+  "id" UUID PRIMARY KEY DEFAULT gen_random_uuid(),
   "title" VARCHAR(255) NOT NULL,
   "author" VARCHAR(255) NOT NULL,
   "published_year" INT NOT NULL,
@@ -24,7 +24,7 @@ CREATE TABLE "books" (
 );
 
 CREATE TABLE "members" (
-  "id" UUID PRIMARY KEY,
+  "id" UUID PRIMARY KEY DEFAULT gen_random_uuid(),
   "name" VARCHAR(255) NOT NULL,
   "email" VARCHAR(255) UNIQUE NOT NULL,
   "phone" VARCHAR(15) NOT NULL,
@@ -34,7 +34,7 @@ CREATE TABLE "members" (
 );
 
 CREATE TABLE "borrowings" (
-  "id" UUID PRIMARY KEY,
+  "id" UUID PRIMARY KEY DEFAULT gen_random_uuid(),
   "book_id" UUID NOT NULL,
   "member_id" UUID NOT NULL,
   "borrow_date" DATE NOT NULL,
